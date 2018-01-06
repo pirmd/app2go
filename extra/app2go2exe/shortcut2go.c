639eaa21296082182f836f2fc0208962b39e02fd
Πrmd <pirmd@users.noreply.github.com>
2018-01-06 08:06:53 +0100

    update author information with up-to-date mail

--- extra/app2go2exe/shortcut2go.c
+++ extra/app2go2exe/shortcut2go.c
@@ -1,5 +1,6 @@
 /**
-   Copyright (c) 2014, Pierre Duranton <pmd-prj@laposte.net>
+   Copyright (c) 2014, Πrmd <pirmd@users.noreply.github.com>
+
    All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
